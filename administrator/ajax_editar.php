1500522376ed7487780eda31097f93b14b175fde
Andre Matos <andre_matos13@hotmail.com>
2015-07-29 17:30:17 -0300

    Adicionando ajax_editar

---
+++ administrator/ajax_editar.php
@@ -0,0 +1,18 @@
+<div class="formulario">
+	<?php if($_GET['tipo'] == 1){ ?>
+		<form method="POST">
+			<span>Digite um nome para editar:</span><br />
+			<span>Nome: </span><input type="text" id="nome_search" name="nome_search"><input type="submit">
+		</form>
+	<?php }else if($_GET['tipo'] == 2){ ?>
+		<form method="POST">
+			<span>Digite um nome para excluir:</span><br />
+			<span>Nome: </span><input type="text" id="nome_search" name="nome_search"><input type="submit">
+		</form>	
+	<?php }else{ ?>
+		<form method="POST">
+			<span>Digite um nome para  pesquisar:</span><br />
+			<span>Nome: </span><input type="text" id="nome_search" name="nome_search"><input type="submit">
+		</form>
+	<?php } ?>
+</div>
