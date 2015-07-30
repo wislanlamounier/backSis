4cfab488a46d4af75e73544868d0e51936a19b51
Andre Matos <andre_matos13@hotmail.com>
2015-07-30 14:23:00 -0300

    Alterando ajax_buscar_informacoes para retornar um link

--- administrator/ajax_buscar_informacoes.php
+++ administrator/ajax_buscar_informacoes.php
@@ -6,26 +6,28 @@ include_once("../model/class_funcionario_bd.php");
 	$sql = new Sql();
 	$sql->conn_bd();
 
-	$id = $_GET['id_funcionario'];  //codigo do estado passado por parametro
+	$nome = $_GET['nome'];  //codigo do estado passado por parametro
 
-	$sql = "SELECT * FROM funcionario WHERE id = $id ORDER BY id";  //consulta todas as cidades que possuem o codigo do estado
+	$sql = "SELECT * FROM funcionario WHERE nome LIKE '%$nome%' && oculto = 0 ORDER BY id";  //consulta todas as cidades que possuem o codigo do estado
 	$res = mysql_query($sql);
 	$num = mysql_num_rows($res);
 	//monto um array de cidades
 	for ($i = 0; $i < $num; $i++) {
 	  $dados = mysql_fetch_array($res);
-	  $arrFuncionario[$dados['id']] = $dados['nome'];
+	  $arrFuncionario[$i][0] = $dados['id'];
+	  $arrFuncionario[$i][1] = $dados['nome'];
 	}
 ?>
 
-
+<div class="formulario">
 <table>
   <?php
   	if($dados) 
 	    foreach($arrFuncionario as $value => $nome){
-	      echo "<tr><td>{$nome}</td></tr>";
+	      echo "<tr><td><a href='add_func.php?tipo=editar&id=".$arrFuncionario[$value][0]."'>".$arrFuncionario[$value][1]."</a></td></tr>";
 	     // echo "<option>teste</option>";
 	  	}
 	
 ?>
 </table>
+</div>
