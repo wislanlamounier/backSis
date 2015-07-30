668a7867d0e4449a25584945a56834991ee1c4e1
Andre Matos <andre_matos13@hotmail.com>
2015-07-30 16:36:57 -0300

    Alterando layout do topo

--- view/menu_admin.php
+++ view/menu_admin.php
@@ -1,18 +1,18 @@
 <ul class="menu-list">  
     <li><a href='../administrator/principal.php'>Início</a></li>
     <li>
-      <a>Cadastrar</a>
+      <!-- color:#000; text-shadow: 0px 0px 5px #000; font-size: 13px; padding-left:3px;padding-right:3px; background-color:rgba(255,255,255,0.5) -->
+      <a style=""><span style="float:left">Cadastrar </span><img style="float:left; margin-top:-2px; margin-left:2px;" src="../images/downarrow_1246.png" width="20px;"></a>
        <ul class="sub-menu">
         <li><a href="add_func.php">Funcionário</a></li>
         <li><a href="add_cliente.php" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all">Cliente</a></li>
-        <li><a href="#">Nova Janela de Exemplo</a></li>
         <li><a href="add_turno.php">Turno</a></li>
         <li><a href="add_cbo.php">CBO</a></li>
         <li><a href="add_exames.php">Exames</a></li>
       </ul>
     </li>
     <li>
-      <a>Pesquisar</a>
+      <a style=""><span style="float:left">Pesquisar </span><img style="float:left; margin-left:2px; margin-top:-2px;" src="../images/downarrow_1246.png" width="20px;"></a>
        <ul class="sub-menu">
         <li><a href="pesquisa_func.php">Funcionário</a></li>
         <li><a href="pesquisa_empresa.php">Empresa</a></li>
