25aad0ac722d729ab3f6f3a18c263fec4b268e5f
Andre Matos <andre_matos13@hotmail.com>
2015-07-30 14:24:17 -0300

    Criando ajax que retorna tela de busca

--- administrator/ajax_editar.php
+++ administrator/ajax_editar.php
@@ -2,17 +2,20 @@
 	<?php if($_GET['tipo'] == 1){ ?>
 		<form method="POST">
 			<span>Digite um nome para editar:</span><br />
-			<span>Nome: </span><input type="text" id="nome_search" name="nome_search"><input type="submit">
+			<span>Nome: </span>
+			<input type="text" id="nome_search" name="nome_search"><input type="button" value="Buscar" onclick="buscar_funcionarios()">
 		</form>
 	<?php }else if($_GET['tipo'] == 2){ ?>
 		<form method="POST">
 			<span>Digite um nome para excluir:</span><br />
-			<span>Nome: </span><input type="text" id="nome_search" name="nome_search"><input type="submit">
+			<span>Nome: </span>
+			<input type="text" id="nome_search" name="nome_search"><input type="button" value="Buscar" onclick="buscar_funcionarios()">
 		</form>	
 	<?php }else{ ?>
 		<form method="POST">
 			<span>Digite um nome para  pesquisar:</span><br />
-			<span>Nome: </span><input type="text" id="nome_search" name="nome_search"><input type="submit">
+			<span>Nome: </span>
+			<input type="text" id="nome_search" name="nome_search"><input type="button" value="Buscar" onclick="buscar_funcionarios()">
 		</form>
 	<?php } ?>
 </div>
