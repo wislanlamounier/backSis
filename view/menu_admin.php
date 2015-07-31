170948e0556a9ddf21323b2b6b3cc803805da3cb
Andre Matos <andre_matos13@hotmail.com>
2015-07-31 16:27:59 -0300

    Alterando menu_admin

--- view/menu_admin.php
+++ view/menu_admin.php
@@ -2,7 +2,7 @@
     <li style="border-left: 1px solid#565656;"><a href='../administrator/principal.php'>Início</a></li>
     <li>
       <!-- color:#000; text-shadow: 0px 0px 5px #000; font-size: 13px; padding-left:3px;padding-right:3px; background-color:rgba(255,255,255,0.5) -->
-      <a style=""><span style="float:left">Cadastrar </span><img style="float:left; margin-top:-2px; margin-left:2px;" src="../images/downarrow_1246.png" width="20px;"></a>
+      <a style=""><span style="float:left">Cadastrar </span><img style="float:left; margin-top:-2px; margin-left:2px;" src="../images/downarrow_1246.png" width="15px;"></a>
        <ul class="sub-menu">
         <li><a href="add_func.php">Funcionário</a></li>
         <li><a href="add_cliente.php" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all">Cliente</a></li>
@@ -12,7 +12,7 @@
       </ul>
     </li>
     <li>
-      <a style=""><span style="float:left">Pesquisar </span><img style="float:left; margin-left:2px; margin-top:-2px;" src="../images/downarrow_1246.png" width="20px;"></a>
+      <a style=""><span style="float:left">Pesquisar </span><img style="float:left; margin-left:2px; margin-top:-2px;" src="../images/downarrow_1246.png" width="15px;"></a>
        <ul class="sub-menu">
         <li><a href="pesquisa_func.php">Funcionário</a></li>
         <li><a href="pesquisa_empresa.php">Empresa</a></li>
