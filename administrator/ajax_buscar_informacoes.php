f153f32196d99c9027330a51d9099fa3d8b03938
Andre Matos <andre_matos13@hotmail.com>
2015-07-31 11:03:31 -0300

    Criando box msg

--- administrator/ajax_buscar_informacoes.php
+++ administrator/ajax_buscar_informacoes.php
@@ -12,6 +12,10 @@ include_once("../model/class_funcionario_bd.php");
 	$res = mysql_query($sql);
 	$num = mysql_num_rows($res);
 	//monto um array de cidades
+	if($num == 0){
+		echo "<div class='msg' style='margin-top: 20px;'>Nenhum Registro encontrado!</div>";
+		return;
+	}
 	for ($i = 0; $i < $num; $i++) {
 	  $dados = mysql_fetch_array($res);
 	  $arrFuncionario[$i][0] = $dados['id'];
@@ -20,14 +24,22 @@ include_once("../model/class_funcionario_bd.php");
 ?>
 
 <div class="formulario">
-<table>
-  <?php
-  	if($dados) 
-	    foreach($arrFuncionario as $value => $nome){
-	      echo "<tr><td><a href='add_func.php?tipo=editar&id=".$arrFuncionario[$value][0]."'>".$arrFuncionario[$value][1]."</a></td></tr>";
-	     // echo "<option>teste</option>";
-	  	}
-	
-?>
-</table>
+	<div class="msg" style="float:left">
+		<div style="float:left; background-color:rgba(50,200,50,0.3); width:100%; height:43px; text-align:left; margin-top:-20px;">
+			<div style="float:left; margin-left:5px;"><img src="../images/search-icon.png" style="width:40px;"></div>
+			<div style="float:left; margin-left:5px; margin-top:10px; font-size:18px; color:#333;">Funcionários</div>
+		</div>
+		<table style="float:left" class="table-pesquisa">
+		  <?php
+		  	$cont=0;
+		  	if($dados) 
+			    foreach($arrFuncionario as $value => $nome){
+			      echo "<tr><td style='padding-left:20px;'><a href='add_func.php?tipo=editar&id=".$arrFuncionario[$value][0]."'>".$arrFuncionario[$value][1]."</a></td></tr>";
+			     	$cont++;
+			  	}
+			  	echo '<tr><td style="padding-left:20px; font-size: 12px; color:#777;">'.$cont. " registro(s) encontrado(s)</td></tr>";
+		   ?>
+		  
+		</table>
+	</div>
 </div>
