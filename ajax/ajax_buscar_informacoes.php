400bd96437d6b1e856a93a0be511eb38868d1d9e
Andre Matos <andre_matos13@hotmail.com>
2015-08-04 16:38:37 -0300

    Adicionando if para verificar se o parametro recebido é de edição ou exclusão

--- ajax/ajax_buscar_informacoes.php
+++ ajax/ajax_buscar_informacoes.php
@@ -16,6 +16,8 @@ include_once("../model/class_funcionario_bd.php");
 	}
 ?>
 
+<?php if(isset($_GET['param']) && $_GET['param'] == 0){ // EDITAR FUNCIONARIO?>
+
 <div class="formulario" style="width:450px">
 	<div class="msg" style="float:left">
 		<div style="float:left; background-color:rgba(50,200,50,0.3); width:100%; height:43px; text-align:left; margin-top:-20px;">
@@ -36,3 +38,27 @@ include_once("../model/class_funcionario_bd.php");
 		</table>
 	</div>
 </div>
+<?php } else if (isset($_GET['param']) && $_GET['param'] == 1){ // EXCLUIR FUNCIONARIO?>
+<div class="formulario" style="width:450px">
+	<div class="msg" style="float:left">
+		<div style="float:left; background-color:rgba(200,50,50,0.3); width:100%; height:43px; text-align:left; margin-top:-20px;">
+			<div style="float:left; margin-left:5px;"><img src="../images/delete.png" style="width:35px; margin-top:3px;"></div>
+			<div style="float:left; margin-left:5px; margin-top:10px; font-size:18px; color:#333;">Excluir Funcionários <span>(Clique em um registro para excluir)</span></div>
+		</div>
+		<table style="float:left" class="table-pesquisa">
+		  <?php
+		  	$cont=0;
+		  	if($funcionario) 
+			    foreach($arrFuncionario as $value => $nome){
+			      echo "<tr><td style='padding-left:20px;'><a title='Clique para excluir' onclick='confirma(".'"'.$arrFuncionario[$value][0].'"'.",".'"'.$arrFuncionario[$value][1].'"'.")'>".$arrFuncionario[$value][1]."</a></td></tr>";
+			     	$cont++;
+			  	}
+			  	echo '<tr><td style="padding-left:20px; font-size: 12px; color:#777;">'.$cont. " registro(s) encontrado(s)</td></tr>";
+		   ?>
+		  
+		</table>
+	</div>
+</div>
+
+
+<?php } ?>
