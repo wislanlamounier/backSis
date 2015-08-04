77b8d1f2ff166dda85185e7238e3ffc30f70b33a
Andre Matos <andre_matos13@hotmail.com>
2015-08-04 13:28:25 -0300

    Modificando layout do box topo

--- view/topo.php
+++ view/topo.php
@@ -1,8 +1,17 @@
+<?php 
+	if($_SESSION['nivel_acesso'] == 0){
+		$nivel_acesso = "Acesso Total";
+	}else if($_SESSION['nivel_acesso'] == 1){
+		$nivel_acesso = "Acesso ViaCampos";
+	}else{
+		$nivel_acesso = "Acesso ControlPonto";
+	}
+ ?>
 <div style="width:100%; float:left;" >
 	<div class="img" style="float:left"><img src="../images/logo75mm.png"></div>
 	<div class="box-login" style="">
 		<div style="float:left"><img src="../images/user.png" width="40px"></div>
-		<div class="nome-box-login" style=""><span style="">Usuário:<br /></span><?php echo $_SESSION["user"]; ?></div>
+		<div class="nome-box-login" style=""><span style="">Usuário:<br /></span><?php echo $_SESSION["user"]; ?><br /><span style="">Empresa:<br /></span><?php echo $_SESSION['empresa']."<span> (".$nivel_acesso.")</span>" ?></div>
 		<div style="float:right;"><a title="Clique para sair" href="logout.php"><img src="../images/fechar.png" width="20px"></a></div>
 	</div>
 </div>
