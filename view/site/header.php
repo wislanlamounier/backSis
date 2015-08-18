c640b4f208a3c781a01ad6b4d5e3caaa175bb0f8
Andre Matos <andre_matos13@hotmail.com>
2015-08-18 15:49:56 -0300

    Modificando extensão

---
+++ view/site/header.php
@@ -0,0 +1,29 @@
+<a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>ControlSystem</h1>
+                        <!-- <h3>Acesse Agora</h3> -->
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="administrator/" class="btn btn-default btn-lg"><span class="network-name">Acesse agora</span></a>
+                            </li>
+                            <!-- <li>
+                                <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                            </li> -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.intro-header -->
