a4014efdd004599842364e32cca65a74ec806e04
Andre Matos <andre_matos13@hotmail.com>
2015-08-26 12:03:31 -0300

    Dicionario e uml

---
+++ DICIONARIO.php
@@ -0,0 +1,49 @@
+<?
+/**
+* @author Andre Matos
+* @access public
+*
+*/
+OBSERVAÇÕES TABELA BANCO{
+	
+	TABELA(VEICULO, MAQUINARIO, PATRIMONIO_GERAL){
+		CAMPO: CONTROLE{
+			0 -> PATRIMONIO GERAL
+			1 -> MAQUINARIO
+			2 -> VEICULO 
+		}
+		
+	}
+
+	TABELA (EMPRESA){
+		CAMPO: NIVEL_ACESSO{
+			0 -> ACESSO TOTAL
+			1 -> ACESSO ENGENHARIA
+			2 -> ACESSO CONTROLPONTO	
+		}
+		
+	}
+
+	TABLE (CLIENTES){
+		CAMPO: TIPO{
+			0 -> CLIENTE
+			1 -> EMPRESA	
+		}
+		
+	}
+
+	TABLE (EPI){
+		CAMPO: EPI{
+			0-> NAO É EPI
+			1-> É EPI	
+		}
+		
+	}
+}
+
+
+SIGLAS{
+	EPI: EQUIPAMENTO DE PROTEÇÃO INDIVIDUAL
+	CBO: CADASTRO BRASILEIRO DE OCUPAÇÕES
+}
+?>
