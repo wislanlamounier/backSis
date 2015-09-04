04ddb2a82663b13d10c29423058d619bc10dd8c8
LucasSoaresGit <lucasoaresnoggg@gmail.com>
2015-09-04 17:31:24 -0300

    final_dodia

--- administrator/principal.php
+++ administrator/principal.php
@@ -11,7 +11,7 @@ include_once("../model/class_horarios_bd.php");
  	<script src="../javascript/jquery-2.1.4.min.js"></script>
  	<!--<script src="../javascript/jquery_mobile/jquery.mobile-1.4.5.min.js"></script>-->
  	<!-- <link rel="stylesheet" type="text/css" href="../javascript/jquery_mobile/jquery.mobile-1.4.5.min.css"> -->
- 	<link rel="stylesheet" type="text/css" href="style.css">
+ 	<link rel="stylesheet" type="text/css" href="style1.css">
  </head> 
  <script type="text/javascript">
  function moveRelogio(){
@@ -38,9 +38,11 @@ include_once("../model/class_horarios_bd.php");
  		<?php include_once("../view/topo.php"); ?>
  		<?php if($_SESSION['nivel_acesso'] == 0 || $_SESSION['nivel_acesso'] == 2){
  			echo '<div class="formulario" style="width:93%">';
- 			include_once("../view/box-atrasos.php");
- 			echo '</div>';
- 		} ?>
+ 			// include_once("../view/box-atrasos.php");
+ 			include("../view/painel_cliente.php");
+
+ 		}?> 
+
  		
  </body>
  </html>
