d7c56a22dca73eebd8acf1a8e1fa43d401781a97
Andre Matos <andre_matos13@hotmail.com>
2015-09-22 17:02:12 -0300

    Excluir produto

--- ajax/ajax_apagar.php
+++ ajax/ajax_apagar.php
@@ -14,46 +14,85 @@ include_once("../model/class_produto_bd.php");
   $acao = isset($_GET['acao']) ? $_GET['acao'] : null;
 	$total = 0;
 	$cont = 0;
-        echo "<script>alert('Acao: ".$acao."');</script>";
+        
         if($whatarray == 'material'){// se for igual produto exclui os materiais do adicionar produto
-            
-            for($aux = 0; $aux < count($_SESSION['produto'][$whatarray]); $aux++){//percorrendo array escolhido
-            
-              if($_SESSION['produto'][$whatarray][$aux] == $id){// quando encontrar o id escolhido
+              if($acao == 'cadastrar'){// se for cadastrar exclui a session do cadastrar
+                  for($aux = 0; $aux < count($_SESSION['produto'][$whatarray]); $aux++){//percorrendo array escolhido
+                  
+                    if($_SESSION['produto'][$whatarray][$aux] == $id){// quando encontrar o id escolhido
 
-                  unset($_SESSION['produto'][$whatarray][$aux]);// unset posição do array que contem o id à ser excluido
-              }
+                        unset($_SESSION['produto'][$whatarray][$aux]);// unset posição do array que contem o id à ser excluido
+                    }
+                  }
+                  foreach ($_SESSION['produto'][$whatarray] as $key => $value) { // percorre o array e reordena-o
+                     $array[$cont] = $value;
+                     $cont++;
+                  }
+
+                  if(isset($array) && count($array > 0))//se existir array
+                    $_SESSION['produto'][$whatarray] = $array;// session = array
+
+                  echo '<table style="width:100%" >';
+                    for($aux = 0; $aux < count($_SESSION['produto'][$whatarray]); $aux++){ // percorre array de material       
+                        if($aux%2==0)//if else para tabela zebrada
+                           echo '<tr style="background-color:#ccc;">';
+                        else
+                           echo '<tr style="background-color:#ddd;">';
+                                 $id_qtd_tipo = explode(":",$_SESSION['produto'][$whatarray][$aux]); // separa os valores id e quantidade
+                              if($id_qtd_tipo[2] == 'm'){// se for material
+                                   $res = new Material();
+                                   $res = Material::get_material_id($id_qtd_tipo[0]);
+                                   $uni = new Unidade_medida();
+                                   $uni = $uni->get_unidade_medida_by_id($res->id_unidade_medida);
+                                   echo '<td ><span>'.$res->nome.': </span></td><td><input  id="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" onchange="increment(this.id)" style="width:30%; background-color: rgba(230,230,230,0.5)" type="number" value="'.$id_qtd_tipo[1].'"> <span>'.$uni->sigla.'</span></td><td><a name="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" style="cursor:pointer"  onclick="apagar(this.name,\'material\')"><img style="width:15px" src="../images/delete.png"></a></td>';
+                              }else if($id_qtd_tipo[2] == 'p'){// se for produto
+                                   $res = new Produto();
+                                   $res = $res->get_produto_id($id_qtd_tipo[0]);
+                                   echo '<td ><span>'.$res->nome.': </span></td><td><input  id="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" onchange="increment(this.id)" style="width:30%; background-color: rgba(230,230,230,0.5)" type="number" value="'.$id_qtd_tipo[1].'"></td><td><a name="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" style="cursor:pointer"  onclick="apagar(this.name,\'material\')"><img style="width:15px" src="../images/delete.png"></a></td>';
+                              }
+                              
+                          echo '</tr>';
+                    }
+                    echo '</table>';
+            }else{// se for editar exclui a session do editar
+                  for($aux = 0; $aux < count($_SESSION['produto']['editar'][$whatarray]); $aux++){//percorrendo array escolhido
+                  
+                    if($_SESSION['produto']['editar'][$whatarray][$aux] == $id){// quando encontrar o id escolhido
+
+                        unset($_SESSION['produto']['editar'][$whatarray][$aux]);// unset posição do array que contem o id à ser excluido
+                    }
+                  }
+                  foreach ($_SESSION['produto']['editar'][$whatarray] as $key => $value) { // percorre o array e reordena-o
+                     $array[$cont] = $value;
+                     $cont++;
+                  }
+
+                  if(isset($array) && count($array > 0))//se existir array
+                    $_SESSION['produto']['editar'][$whatarray] = $array;// session = array
+
+                  echo '<table style="width:100%" >';
+                    for($aux = 0; $aux < count($_SESSION['produto']['editar'][$whatarray]); $aux++){ // percorre array de material       
+                        if($aux%2==0)//if else para tabela zebrada
+                           echo '<tr style="background-color:#ccc;">';
+                        else
+                           echo '<tr style="background-color:#ddd;">';
+                                 $id_qtd_tipo = explode(":",$_SESSION['produto']['editar'][$whatarray][$aux]); // separa os valores id e quantidade
+                              if($id_qtd_tipo[2] == 'm'){// se for material
+                                   $res = new Material();
+                                   $res = Material::get_material_id($id_qtd_tipo[0]);
+                                   $uni = new Unidade_medida();
+                                   $uni = $uni->get_unidade_medida_by_id($res->id_unidade_medida);
+                                   echo '<td ><span>'.$res->nome.': </span></td><td><input  id="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" onchange="increment(this.id)" style="width:30%; background-color: rgba(230,230,230,0.5)" type="number" value="'.$id_qtd_tipo[1].'"> <span>'.$uni->sigla.'</span></td><td><a name="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" style="cursor:pointer"  onclick="apagar(this.name,\'material\')"><img style="width:15px" src="../images/delete.png"></a></td>';
+                              }else if($id_qtd_tipo[2] == 'p'){// se for produto
+                                   $res = new Produto();
+                                   $res = $res->get_produto_id($id_qtd_tipo[0]);
+                                   echo '<td ><span>'.$res->nome.': </span></td><td><input  id="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" onchange="increment(this.id)" style="width:30%; background-color: rgba(230,230,230,0.5)" type="number" value="'.$id_qtd_tipo[1].'"></td><td><a name="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" style="cursor:pointer"  onclick="apagar(this.name,\'material\')"><img style="width:15px" src="../images/delete.png"></a></td>';
+                              }
+                              
+                          echo '</tr>';
+                    }
+                    echo '</table>';
             }
-            foreach ($_SESSION['produto'][$whatarray] as $key => $value) { // percorre o array e reordena-o
-               $array[$cont] = $value;
-               $cont++;
-            }
-
-            if(isset($array) && count($array > 0))//se existir array
-              $_SESSION['produto'][$whatarray] = $array;// session = array
-
-            echo '<table style="width:100%" >';
-              for($aux = 0; $aux < count($_SESSION['produto'][$whatarray]); $aux++){ // percorre array de material       
-                  if($aux%2==0)//if else para tabela zebrada
-                     echo '<tr style="background-color:#ccc;">';
-                  else
-                     echo '<tr style="background-color:#ddd;">';
-                           $id_qtd_tipo = explode(":",$_SESSION['produto'][$whatarray][$aux]); // separa os valores id e quantidade
-                        if($id_qtd_tipo[2] == 'm'){// se for material
-                             $res = new Material();
-                             $res = Material::get_material_id($id_qtd_tipo[0]);
-                             $uni = new Unidade_medida();
-                             $uni = $uni->get_unidade_medida_by_id($res->id_unidade_medida);
-                             echo '<td ><span>'.$res->nome.': </span></td><td><input  id="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" onchange="increment(this.id)" style="width:30%; background-color: rgba(230,230,230,0.5)" type="number" value="'.$id_qtd_tipo[1].'"> <span>'.$uni->sigla.'</span></td><td><a name="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" style="cursor:pointer"  onclick="apagar(this.name,\'material\')"><img style="width:15px" src="../images/delete.png"></a></td>';
-                        }else if($id_qtd_tipo[2] == 'p'){// se for produto
-                             $res = new Produto();
-                             $res = $res->get_produto_id($id_qtd_tipo[0]);
-                             echo '<td ><span>'.$res->nome.': </span></td><td><input  id="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" onchange="increment(this.id)" style="width:30%; background-color: rgba(230,230,230,0.5)" type="number" value="'.$id_qtd_tipo[1].'"></td><td><a name="'.$res->id.':'.$id_qtd_tipo[1].':'.$id_qtd_tipo[2].'" style="cursor:pointer"  onclick="apagar(this.name,\'material\')"><img style="width:15px" src="../images/delete.png"></a></td>';
-                        }
-                        
-                    echo '</tr>';
-              }
-              echo '</table>';
 
 
         }else{ // se não, exclui o array de obra
