153f9e645dbc3e0cc89d8f24805b51fd518ac929
Andre Matos <andre_matos13@hotmail.com>
2015-09-23 09:00:03 -0300

    Adicionando novo BD e excluindo BD antigo

--- bd_viacampus 23-09-2015 (zerado).sql
+++ bd_viacampus 23-09-2015 (zerado).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 21-Ago-2015 às 20:16
--- Versão do servidor: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: 23-Set-2015 às 13:54
+-- Versão do servidor: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bd_viacampos`
+-- Database: `bd_viacampus`
 --
 
 -- --------------------------------------------------------
@@ -32,20 +32,23 @@ CREATE TABLE IF NOT EXISTS `veiculo` (
   `chassi` varchar(56) NOT NULL,
   `renavam` int(11) NOT NULL,
   `placa` varchar(8) NOT NULL,
-  `marca` varchar(56) NOT NULL,
+  `id_marca` int(11) NOT NULL,
   `modelo` varchar(56) NOT NULL,
   `ano` int(11) NOT NULL,
-  `cor` varchar(56) NOT NULL,
-  `valor` int(11) NOT NULL,
+  `id_cor` int(11) NOT NULL,
+  `valor` double NOT NULL,
   `data_compra` date NOT NULL,
+  `data_ini_seg` date NOT NULL,
+  `data_fim_seg` date NOT NULL,
   `seguro` int(11) NOT NULL,
-  `quilometragem` int(11) NOT NULL,
   `km_inicial` int(20) NOT NULL,
   `tipo_combustivel` varchar(56) NOT NULL,
   `id_empresa` int(11) NOT NULL,
   `id_fornecedor` int(11) NOT NULL,
   `id_responsavel` int(11) NOT NULL,
-  `oculto` int(11) NOT NULL
+  `oculto` int(11) NOT NULL,
+  `ativo` int(11) NOT NULL,
+  `controle` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -56,7 +59,9 @@ CREATE TABLE IF NOT EXISTS `veiculo` (
 -- Indexes for table `veiculo`
 --
 ALTER TABLE `veiculo`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `matricula` (`matricula`),
+  ADD UNIQUE KEY `chassi` (`chassi`,`renavam`,`placa`);
 
 --
 -- AUTO_INCREMENT for dumped tables
